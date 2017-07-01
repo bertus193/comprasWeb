c8c344702c946239c97c87a5a2b93bb25fc73917
Alberto Rico Garcia <albrigar93@gmail.com>
2017-07-01 18:22:02 +0200

    SQL Injection CodeIgniter prevention

--- application/models/app.php
+++ application/models/app.php
@@ -14,8 +14,8 @@ class App extends CI_Model
     public function getSubastas($start, $per_page)
     {
         $subastas = array();
-        $query = $this->db->query("SELECT id, personaje, creditos, precioTipo, precio, fechaFin, now() as now FROM comercio WHERE fechaFin > now() AND finalizada = 0 ORDER BY (precio/creditos),id ASC LIMIT $start, $per_page");
-
+        $sql = 'SELECT id, personaje, creditos, precioTipo, precio, fechaFin, now() as now FROM comercio WHERE fechaFin > now() AND finalizada = 0 ORDER BY (precio/creditos),id ASC LIMIT ?, ?';
+        $query = $this->db->query($sql, array($start, $per_page));
         foreach ($query->result() as $oferta) {
             $oferta = new Subasta($oferta->id, $oferta->personaje, $oferta->creditos, $oferta->precioTipo, $oferta->precio, $oferta->fechaFin, $oferta->now);
             array_push($subastas, $oferta);
@@ -27,7 +27,8 @@ class App extends CI_Model
     public function getSubastasFinalizadas($total)
     {
         $subastas = array();
-        $query = $this->db->query("SELECT id, compradorPjNombre , personaje, creditos, precioTipo, precio, fechaFin, compradorCuenta, now() as now FROM comercio WHERE fechaCompra is not null ORDER BY fechaCompra desc LIMIT 6");
+        $sql = "SELECT id, compradorPjNombre , personaje, creditos, precioTipo, precio, fechaFin, compradorCuenta, now() as now FROM comercio WHERE fechaCompra is not null ORDER BY fechaCompra desc LIMIT ?";
+        $query = $this->db->query($sql, array($total));
         foreach ($query->result() as $oferta) {
             $oferta = new Subasta($oferta->id, $oferta->personaje, $oferta->creditos, $oferta->precioTipo, $oferta->precio, $oferta->fechaFin, $oferta->now);
             array_push($subastas, $oferta);
